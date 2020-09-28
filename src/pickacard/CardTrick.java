8b4bc08519ccb750fa57a0d2aebf679953cf2889
Gabriel <espingab@sheridancollege.ca>
2020-09-28 19:48:10 -0400

    First Commit for first test

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -18,7 +18,7 @@ public class CardTrick {
             //c.setValue(insert call to random number generator here)
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
             
-            // Commit to the right branch
+            // Commit to the branch
         }
 
         //insert code to ask the user for Card value and suit, create their card
