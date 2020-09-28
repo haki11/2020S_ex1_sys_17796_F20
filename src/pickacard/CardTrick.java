83cb3c4a0a93563cfa51e936bbd6e032f2ebd06b
Gabriel <espingab@sheridancollege.ca>
2020-09-28 19:38:36 -0400

    First Commit for first test

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -17,6 +17,8 @@ public class CardTrick {
             Card c = new Card();
             //c.setValue(insert call to random number generator here)
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            
+            // Make sure it is all good
         }
 
         //insert code to ask the user for Card value and suit, create their card
