4812ceb5ca229b188a4d4d6bdd57fc0bb0850f34
Krista <powellkr@sheridancollege.ca>
2020-09-29 19:04:55 -0400

    author commit

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -1,5 +1,5 @@
 package pickacard;
-
+//Krista Powell
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects and then asks the user to pick a card and
  * searches the array of cards for the match to the user's card. To be used as starting code in ICE 1
@@ -7,21 +7,23 @@ package pickacard;
  * @author dancye
  * @author Paul Bonenfant
  */
-public class CardTrick {
+public class CardTrick
+{
 
-    public static void main(String[] args) {
-        
-        Card[] magicHand = new Card[7];
+   public static void main (String[] args)
+   {
 
-        for (int i = 0; i < magicHand.length; i++) {
-            Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
-        }
+      Card[] magicHand = new Card[7];
 
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
-    }
+      for (int i = 0; i < magicHand.length; i++) {
+         Card c = new Card();
+         //c.setValue(insert call to random number generator here)
+         //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+      }
+
+      //insert code to ask the user for Card value and suit, create their card
+      // and search magicHand here
+      //Then report the result here
+   }
 
 }
