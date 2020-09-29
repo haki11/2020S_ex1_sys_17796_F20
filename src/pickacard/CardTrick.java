68770d33564b268d2948060d8c3a8512fff67f67
Ahmed <Ahmed@192.168.2.196>
2020-09-29 19:49:23 -0400

    first commit

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -22,6 +22,9 @@ public class CardTrick {
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
+        
+        
+        // this is a test
     }
 
 }
