089f8ab2cd03696c89fd11b1dc4f97e7a684ba7c
naoldenko <naoldenko@gmail.com>
2020-09-29 19:47:50 -0400

    Testing

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -17,6 +17,7 @@ public class CardTrick {
             Card c = new Card();
             //c.setValue(insert call to random number generator here)
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            // My name is Naol Denko
         }
 
         //insert code to ask the user for Card value and suit, create their card
