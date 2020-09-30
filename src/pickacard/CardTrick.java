fe30ee47822cccec63863351c366a1139ed5731b
ndsheridan <denkon@sheridancollege.ca>
2020-09-29 22:09:36 -0400

    Implemented Task #3 - Forgot To Make One of the Cards in the Magic Hand Equal The Randomly Generated Card

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -1,5 +1,4 @@
 package pickacard;
-
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects and then asks the user to pick a card and
  * searches the array of cards for the match to the user's card. To be used as starting code in ICE 1
@@ -16,6 +15,7 @@ public class CardTrick {
             Card c = new Card();  
             c.setValue(c.randomNumber());
             c.setSuit(Card.SUITS[c.randomSuit()]);
+            magicHand[i] = c;
         }
         
         //insert code to ask the user for Card value and suit, create their card
