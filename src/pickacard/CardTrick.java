a984e792f4646c333049e5ce880553de01d5d1e4
ndsheridan <denkon@sheridancollege.ca>
2020-09-29 20:26:33 -0400

    Testing

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -4,8 +4,6 @@ package pickacard;
  * A class that fills a magic hand of 7 cards with random Card Objects and then asks the user to pick a card and
  * searches the array of cards for the match to the user's card. To be used as starting code in ICE 1
  *
- * @author dancye
- * @author Paul Bonenfant
  */
 public class CardTrick {
 
@@ -18,6 +16,9 @@ public class CardTrick {
             //c.setValue(insert call to random number generator here)
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
         }
+        //insert code to ask the user for Card value and suit, create their card
+        // and search magicHand here
+        //Then report the result here
     }
 
 }
