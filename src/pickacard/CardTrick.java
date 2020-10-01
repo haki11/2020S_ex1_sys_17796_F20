0bd854a72d9369eb82ecaf39ed58422a0649151e
Gabriel <espingab@sheridancollege.ca>
2020-10-01 16:50:45 -0400

    Finished code for the CardTrick class

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -1,29 +1,52 @@
 package pickacard;
 
+import java.util.*;
+
 /**
- * A class that fills a magic hand of 7 cards with random Card Objects and then asks the user to pick a card and
- * searches the array of cards for the match to the user's card. To be used as starting code in ICE 1
+ * A class that fills a magic hand of 7 cards with random Card Objects and then
+ * asks the user to pick a card and searches the array of cards for the match to
+ * the user's card. To be used as starting code in ICE 1
  *
- * @author dancye
- * @author Paul Bonenfant
+ * @author Gabriel Espinoza 
+ * @date: Oct 4 2020
  */
 public class CardTrick {
 
     public static void main(String[] args) {
-        
+
+        Scanner input = new Scanner(System.in);
+
         Card[] magicHand = new Card[7];
 
+        Random random = new Random();
+        Card c = new Card();
+        
         for (int i = 0; i < magicHand.length; i++) {
-            Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
-            
-            // Commit to the branch
+
+            c.setValue(1 + random.nextInt(14));
+            c.setSuit(Card.SUITS[0 + random.nextInt(3)]);
+
         }
 
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
+        System.out.println("Hello User\nWhat is the card value you want?");
+        int userValue = input.nextInt();
+
+        System.out.println("What is the suit you want?");
+        String userSuit = input.next();
+
+        Card userCard = new Card();
+        userCard.setSuit(userSuit);
+        userCard.setValue(userValue);
+
+        System.out.println(userCard.getValue());
+        System.out.println(userCard.getSuit());
+
+        if (userCard.getValue() == c.getValue() && userCard.getSuit().equals(c.getSuit())) {
+            System.out.println("The cards are the same!\nYOU WIN");
+        } else {
+            System.out.println("The cards are not the same!\nYOU LOSE");
+        }
+
     }
 
 }
