e47c2fb7a08c1cb7a3fa4c4adcd485dc788321f8
Annalpreet Kaur Deol <deolann@sheridancollege.ca>
2020-10-02 19:05:42 -0400

    initial change the code

--- src/pickacard/Card.java
+++ src/pickacard/Card.java
@@ -5,17 +5,20 @@ package pickacard;
  * a value (note that Ace = 1, Jack =11, Queen =12, King = 13)
  * A suit (clubs, hearts, spades, diamonds).
  * There are 52 cards in a deck, no jokers.
- * This code is to be used in Exercise 1. When you create your own branch,
- * add your name as an author.
- * @author dancye
- * @author Paul Bonenfant
+ * 
+ * @author Annal Deol
+ * @date 10/2/2020
  */
 public class Card {
 
-   private String suit; //clubs, spades, diamonds, hearts
-   private int value;//1-13
+  private  String suit;
+private int value;
+    
+    public static void main(String[] args) {
+        Card[] magic = new Card[7];
 
-   public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
+  
+    public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
     /**
      * @return the suit
      */
--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -1,25 +1,67 @@
 package pickacard;
 
 /**
- * A class that fills a magic hand of 7 cards with random Card Objects and then asks the user to pick a card and
- * searches the array of cards for the match to the user's card. To be used as starting code in ICE 1
+ * A class that fills a magic hand of 7 cards with random Card Objects and then asks the user to pick a card
  *
- * @author dancye
- * @author Paul Bonenfant
+ * 
+ * @author Annal Deol
+ * @date 10/2/2020
  */
 public class CardTrick {
 
     public static void main(String[] args) {
-        
-        Card[] magicHand = new Card[7];
+        Card[] goldHand = new Card[7];
+        String randomValue = null;
+        int[] card = {1,2,3,4,5,6,7};
+        final String[]  SUIT=  {"Hearts", "Diamonds", "Spades", "Clubs"};
 
-        for (int i = 0; i < magicHand.length; i++) {
+        for (Card starCard1 : starCard1) {
+            
             Card c = new Card();
+            
             //c.setValue(insert call to random number generator here)
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+        
+            int numVal1 = card [(int)(Math.random()*card.length)];
+            String suitVal1= SUIT[(int) (Math.random()*SUIT.length )];
+            
+            c.setValue(numVal1);
+            c.setSuit(suitVal1);
+            
+            randomValue = numVal1 + " " + suitVal1;
+            
+            System.out.println(randomValue);
         }
+        
+        
+    
+    Scanner input = new Scanner(System.in);
+        //insert code to ask the user for Card value and suit, create their card
+        System.out.println("Enter card of wish");
+        
+      String userVal=input.nextLine();
+      if(randomValue.equalsIgnoreCase(userVal)) {
+          System.out.println("Win");
+      } else
+     System.out.println("lose");
+     
+  
+   
 
-      // I edit directly on github
-    }
+        //Then report the result here
+    int cards=7;
+    String suit="spades";
+    String cardOfMyWish= starCard(card,suit);
+    System.out.println("Card of my wish :"+cardOfMyWish);
+}
+     public static String starCard(int number,String suit) {
+    Card c = new Card();
+    int num =4;
+    String suits = "hearts";
+    String value = num+ " " + suits;
+    return value;
+}     
 
 }
+
+
