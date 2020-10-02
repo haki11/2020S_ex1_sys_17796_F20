3df3cb3492939bd37dfecd90c1e20a1b3eef615a
Ahmed <Ahmed@192.168.2.196>
2020-10-02 15:12:36 -0400

    added steps 3 - 5

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -1,11 +1,14 @@
 package pickacard;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects and then asks the user to pick a card and
  * searches the array of cards for the match to the user's card. To be used as starting code in ICE 1
  *
- * @author dancye
- * @author Paul Bonenfant
+ * @author Ahmed Hashmi
+ * Modified: 9/30/2020
  */
 public class CardTrick {
 
@@ -13,18 +16,59 @@ public class CardTrick {
         
         Card[] magicHand = new Card[7];
 
+        Random dice = new Random();
+        int num;
+        
         for (int i = 0; i < magicHand.length; i++) {
             Card c = new Card();
+            
+            num = dice.nextInt(13);
+            
             //c.setValue(insert call to random number generator here)
+            c.setValue(num);
+            
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setSuit(Card.SUITS[dice.nextInt(3)]);
+            
+            magicHand[i] = c;
         }
 
         //insert code to ask the user for Card value and suit, create their card
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Enter your guess for the value of the card (1-13): ");
+        int userValue = input.nextInt();
+        
+        System.out.println("Enter your guess for the suit of the card (0-3): ");
+        int userSuit = input.nextInt();
+        
+        Card userC = new Card();
+        userC.setValue(userValue);
+        userC.setSuit(Card.SUITS[userSuit]);
+        
+        System.out.println("Your card value is: " + userC.getValue());
+        System.out.println("Your card suit is: " + userC.getSuit());
+        
+        //  This just prints the magic hand
+        
+        for (Card c : magicHand) {
+            System.out.println(c.getValue() + " of " + c.getSuit());
+        }
+        
+        
         // and search magicHand here
-        //Then report the result here
+        for (Card c : magicHand) {
+            if (userC.getValue() == c.getValue() && userC.getSuit().equals(c.getSuit())) {
+                System.out.println("Your card has a match in the magic hand! ");
+                
+            }
+            else {
+                //Then report the result here
+                System.out.println("You did not have a matching card. ");
+            }
+        }
         
         
-        // this is a test
     }
 
 }
