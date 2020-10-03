5f0b7718042ef7b0333e873e1fee00ff57f3d87d
jjdumlao <dumlaojo@sheridancollege.ca>
2020-10-03 18:43:04 -0400

    1st commit

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -1,23 +1,41 @@
 package pickacard;
 
+import java.util.Random;
+import java.util.Scanner;
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects and then asks the user to pick a card and
  * searches the array of cards for the match to the user's card. To be used as starting code in ICE 1
  *
- * @author dancye
- * @author Paul Bonenfant
+ * @author Joshua Dumlao
  */
 public class CardTrick {
 
     public static void main(String[] args) {
         
         Card[] magicHand = new Card[7];
-
-        for (int i = 0; i < magicHand.length; i++) {
+        Random ran = new Random();
+        for (int i=0; i<magicHand.length; i++)
+        {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue(ran.nextInt(12) +1);
+            c.setSuit(Card.SUITS[ran.nextInt(3)]);
+            magicHand[i] = c;
         }
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please choose the suit for your card:");
+        for(int i=0;i<Card.SUITS.length; i++)
+        {
+            System.out.println(i + ": " + Card.SUITS[i]);
+        }
+        int suit = sc.nextInt();
+        
+        System.out.println("Please enter the value of the card you wish to create");
+        int val = sc.nextInt();
+        
+        Card playersCard = new Card();
+        playersCard.setSuit(Card.SUITS[suit]);
+        playersCard.setValue(val);
 
       // I edit directly on github
     }
