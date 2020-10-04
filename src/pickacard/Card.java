27cdf02b99db48c8334bcbd10676d7d36cb0b5f0
Jeremiah Torralba <jeremiahtorralba@gmail.com>
2020-10-04 01:28:16 -0400

    Jeremiah Torralba, 991401572 PickACard Ex1 tasks

--- src/pickacard/Card.java
+++ src/pickacard/Card.java
@@ -1,5 +1,6 @@
 package pickacard;
 
+import java.util.Random;
 /**
  * A class that models playing card Objects. Cards have 
  * a value (note that Ace = 1, Jack =11, Queen =12, King = 13)
@@ -9,38 +10,73 @@ package pickacard;
  * add your name as an author.
  * @author dancye
  * @author Paul Bonenfant
+ * @co-author Jeremiah Torralba
  */
 public class Card {
 
-   private String suit; //clubs, spades, diamonds, hearts
-   private int value;//1-13
-
-   public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
-    /**
+   private Suit suit; //clubs, spades, diamonds, hearts
+   private Value value;//1-13
+   
+    Card() {
+    }
+   
+   public enum Suit {
+           Hearts, Spades, Clubs, Diamonds;
+   };
+   
+   public static Card.Suit genRandSuit() {
+       Suit[] values = Suit.values();
+       int length = values.length;
+       int randIndex = new Random().nextInt(length);
+       return values[randIndex];
+   }
+   
+   public enum Value {
+       Ace(1), Two(2), Three(3), Four(4), Five(5), Six(6), Seven(7), Eight(8), Nine(9), Ten(10), Jack(11), Queen(12), King(13);
+        int val;
+        private Value(int val) {
+            this.val = val;
+        }
+        public static boolean contains(String v) {
+        for(Value value:values())
+           if (value.name().equals(v)) 
+              return true;
+        return false;
+        }   
+   };
+   
+   public static Card.Value genRandValue() {
+       Value[] values = Value.values();
+       int length = values.length;
+       int randIndex = new Random().nextInt(length);
+       return values[randIndex];
+   }
+      
+   /**
      * @return the suit
      */
-    public String getSuit() {
+    public Suit getSuit() {
         return suit;
     }
 
     /**
      * @param suit the suit to set
      */
-    public void setSuit(String suit) {
+    public void setSuit(Suit suit) {
         this.suit = suit;
     }
 
     /**
      * @return the value
      */
-    public int getValue() {
+    public Value getValue() {
         return value;
     }
 
     /**
      * @param value the value to set
      */
-    public void setValue(int value) {
+    public void setValue(Value value) {
         this.value = value;
     }
    
--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -1,25 +1,46 @@
 package pickacard;
 
+import java.util.Scanner;
+import static pickacard.Card.genRandSuit;
+import static pickacard.Card.genRandValue;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects and then asks the user to pick a card and
  * searches the array of cards for the match to the user's card. To be used as starting code in ICE 1
  *
  * @author dancye
  * @author Paul Bonenfant
+ * @co-author Jeremiah Torralba
  */
 public class CardTrick {
-
+  
     public static void main(String[] args) {
         
         Card[] magicHand = new Card[7];
-
-        for (int i = 0; i < magicHand.length; i++) {
-            Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+        Card c = new Card();  
+        
+        Scanner scanner;
+        scanner = new Scanner(System.in);
+        System.out.println("Please pick and enter Card: 'Value of Suit'");
+        System.out.println("");     
+        String value = scanner.next();
+        String suit = scanner.next();
+        System.out.println("");        
+        
+         for (int i = 0; i < magicHand.length; i++) {         
+            c.setValue(genRandValue());
+            c.setSuit(genRandSuit());
+            System.out.println(c.getValue() + " of " + c.getSuit());
+         }   
+        System.out.println(" ");
+             
+        if(genRandValue().contains(value)) {
+            System.out.print("\nYour Card is " + value + " of " + suit);
         }
-
+        
       // I edit directly on github
     }
-
-}
+    
+    
+    
+ }
