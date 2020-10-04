eadcbe6ab0f5312c05d4201c9ebfade8561abc78
muzhang21 <71594798+muzhang21@users.noreply.github.com>
2020-10-04 18:45:37 -0400

    correct way to find whether lucky card is in Array

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -16,32 +16,46 @@ public class CardTrick {
          for (int i = 0; i < magicHand.length; i++) {
               Card c = new Card();
             //c.setValue(insert call to random number generator here)
-               c.setValue((int)(Math.random() * 12));
+               c.setValue((int)(Math.random() * 13 + 1));
            
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
             //it is a String
                c.setSuit(Card.SUITS[(int)(Math.random() * 3)]);
+               magicHand[i] = c;
                System.out.println(c.getValue() + c.getSuit());
         }
-         //ask user to input the value and suit of the lucky card
-         System.out.println("please enter the value and suit of your luckycard");
-         int valueLucky = sc.nextInt();
-         String suitLucky = sc.nextLine();
+        
+          Card luckyCard = new Card();    
+        luckyCard.setValue(12);  
+        luckyCard.setSuit(Card.SUITS[0]);
          
-        //Then report the result and match magicHand[i] here.
-          Card luckyCard = new Card();
-        if(valueLucky == luckyCard.getValue() && suitLucky.equals(luckyCard.getSuit())){
-            System.out.println("you win");}
-        else{
-            System.out.println("you lose");
+        //find the luckyCard whether in magicHand[i].
+          boolean luckyCardfound = false;
+        for(Card c: magicHand)
+        {
+            if((c.getSuit().equals(luckyCard.getSuit())) && (c.getValue()==luckyCard.getValue()))
+            {
+                luckyCardfound = true;
+                break;
+            }
         }
+        if(luckyCardfound)
+            {
+                System.out.println("You win!");
+            }
+        else
+            {
+                System.out.println("you lose.");
+            }
          
          /* I want to replace the code as follow with my luckycard object
         //insert code to ask the user for Card value and suit, create their card
-       System.out.println("please pick any card in the deck");
-        
-         int valuePick = (int)(Math.random() * 12);
-         String suitPick = Card.SUITS[(int)(Math.random() * 3)];
+       
+        //ask user to input the value and suit of the lucky card
+         System.out.println("please enter the value of your card");
+         int valuePick = sc.nextInt();
+         System.out.println("please enter the suit of your card");
+         String suitPick = sc.nextLine();
         
          System.out.println("your card's value is: "+ valuePick);
           System.out.println("your card's suit is: "+ suitPick);
