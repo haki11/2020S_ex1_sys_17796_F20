675cf39464e00f1986979e918aba9b0cf92190e2
Semaj Primm <semajthomasprimm@users.noreply.github.com>
2020-10-04 16:07:15 -0400

    Implemented random card value generator in CardTrick.java

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -1,11 +1,13 @@
 package pickacard;
 
+import java.util.Random;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects and then asks the user to pick a card and
  * searches the array of cards for the match to the user's card. To be used as starting code in ICE 1
  *
- * @author dancye
- * @author Paul Bonenfant
+ * @author Semaj Primm
+ * @date 4 October, 2020
  */
 public class CardTrick {
 
@@ -15,7 +17,13 @@ public class CardTrick {
 
         for (int i = 0; i < magicHand.length; i++) {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
+            // Initalizes r as 
+            Random r = new Random();
+            // Generates a random number between 1-13 inclusively
+            int cardRandomValue = r.nextInt(13)+1;
+            // Sets card value to random number
+            c.setValue(cardRandomValue);
+            
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
         }
 
