07f1b4ab8a866812408f3d5da2f28203a14da284
Ahmad <moneer_bostani@hotmail.com>
2020-10-04 23:11:46 -0400

    Final work. the program is printing the card information successfully

--- src/pickacard/Card.java
+++ src/pickacard/Card.java
@@ -10,40 +10,48 @@ package pickacard;
  * @author dancye
  * @author Paul Bonenfant
  */
-public class Card {
+public class Card
+{
 
    private String suit; //clubs, spades, diamonds, hearts
    private int value;//1-13
 
-   public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
-    /**
-     * @return the suit
-     */
-    public String getSuit() {
-        return suit;
-    }
+   public static final String[] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
 
-    /**
-     * @param suit the suit to set
-     */
-    public void setSuit(String suit) {
-        this.suit = suit;
-    }
+   /**
+    * @return the suit
+    */
+   public String getSuit ()
+   {
+      return suit;
+   }
 
-    /**
-     * @return the value
-     */
-    public int getValue() {
-        return value;
-    }
+   /**
+    * @param suit the suit to set
+    */
+   public void setSuit (String suit)
+   {
+      this.suit = suit;
+   }
 
-    /**
-     * @param value the value to set
-     */
-    public void setValue(int value) {
-        this.value = value;
-    }
-   
-   
-    
+   /**
+    * @return the value
+    */
+   // this  will get the value()
+   public int getValue ()
+   {
+      return value;
+   }
+
+   /**
+    * @param value the value to set
+    */
+   public void setValue (int value)
+   {
+      this.value = value;
+      //setting the value
+   }
+
+
+
 }
--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -1,25 +1,40 @@
 package pickacard;
-
+import java.util.Scanner;
+import java.util.Random;
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects and then asks the user to pick a card and
  * searches the array of cards for the match to the user's card. To be used as starting code in ICE 1
  *
  * @author dancye
  * @author Paul Bonenfant
+ * @author Ahmad Bustani
  */
+
 public class CardTrick {
 
     public static void main(String[] args) {
-        
-        Card[] magicHand = new Card[7];
-
-        for (int i = 0; i < magicHand.length; i++) {
-            Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
-        }
-
-      // I edit directly on github
+    Card[] magicHand = new Card[7];
+    Random random = new Random();
+    Scanner scan = new Scanner(System.in);
+    
+    for(int i = 0; i < magicHand.length; i++) {
+      Card c = new Card();
+      c.setValue(random.nextInt(14)); 
+      c.setSuit(Card.SUITS[random.nextInt(4)]);
+      System.out.printf("Card: %d of %s \n", c.getValue(), c.getSuit());
     }
+    
+    // lucky card
+    Card luckyCard = new Card();
+    System.out.println("Enter a card number: ");
+    luckyCard.setValue(scan.nextInt());
+    scan.nextLine();
+    System.out.println("Enter a card suit: ");
+    luckyCard.setSuit(scan.nextLine());
+    
+    System.out.printf("Your card is: %d of %s \n", luckyCard.getValue(), luckyCard.getSuit());
+ 
+    
 
+  }
 }
