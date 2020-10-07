4849c61314bd5bd4bfcf78c04fa87f2bb6a00aa1
LeeCr <LeeCr@192.168.0.14>
2020-10-06 23:32:50 -0400

    insert random num generators create scanners and check for values/suits in array

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -21,8 +21,34 @@ public class CardTrick {
             c.setValue(num.nextInt(13)+1); //(insert call to random number generator here)
             c.setSUITS(Card.SUITS[num.nextInt(4)]); //[insert call to random number between 0-3 here])
             
-        } 
+        }
         
-    }
-
-}
+        Card inputCard = new Card(); //create defalut user input card object 
+        int Cvalue;
+        String Csuit;
+        boolean found = false;
+        
+        Scanner input = new Scanner(System.in); // get input from user for a card 
+            System.out.print("Please enter a card value between 1-13: ");
+            Cvalue = input.nextInt(); // initialize Cvalue to user input
+            
+            System.out.println("Please enter a card suit (clubs, hearts, spades or diamonds: ");//get user input for card suit
+            Csuit = input.next(); //initialize Csuit to user input 
+            
+            inputCard.setValue(Cvalue); //set valsue for input card
+            inputCard.setSuit(Csuit); //set suit for input card
+            
+            for(int i=0;i<magicHand.length;i++){
+                if(magicHand[i].getValue() == inputCard.getValue()
+                        && magicHand[i].getSuit().equalsIgnoreCase(inputCard.getSuit()))
+                        {
+                            found= true;
+                            break;
+                        }
+                            
+                            
+                }           
+    
+            }
+        
+      } // end of card trick class  
