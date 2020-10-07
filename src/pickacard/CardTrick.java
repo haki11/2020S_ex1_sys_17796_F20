cf63ebb25b90e3bd69268a5f3def927bd78e5bfe
LeeCr <LeeCr@192.168.0.14>
2020-10-06 22:49:34 -0400

    insert random num generators

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -1,5 +1,8 @@
 package pickacard;
 
+import java.util.Scanner;
+import java.util.Random;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects and then asks the user to pick a card and
  * searches the array of cards for the match to the user's card. To be used as starting code in ICE 1
@@ -11,12 +14,15 @@ public class CardTrick {
     public static void main(String[] args) {
         
         Card[] magicHand = new Card[7];
+        Random num = new Random();
 
         for (int i = 0; i < magicHand.length; i++) {
-            Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+           Card c = new Card();
+            c.setValue(num.nextInt(13)+1); //(insert call to random number generator here)
+            c.setSUITS(Card.SUITS[num.nextInt(4)]); //[insert call to random number between 0-3 here])
+            
         } 
+        
     }
 
 }
